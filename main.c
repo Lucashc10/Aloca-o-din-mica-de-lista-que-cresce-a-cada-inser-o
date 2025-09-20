9aa1ad55b2371c573a70bfe4fcb2e92a418dd334
Lucas Henrique Capati <161030534+Lucashc10@users.noreply.github.com>
2025-09-20 15:06:09 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 //Escreva um programa que simule uma lista de números inteiros usando alocação dinâmica
 //(vetor que cresce a cada inserção).
 #include <stdio.h>
